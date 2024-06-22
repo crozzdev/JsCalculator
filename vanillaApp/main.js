daa9ebeac59e6f2dfba65a8dd2f84fa2471ebdfd
CrozzDev <crozzdev95@outlook.com>
2024-06-21 20:48:51 -0500

    started js script

---
+++ vanillaApp/main.js
@@ -0,0 +1,14 @@
+//displays
+const $formulaScreenDiv = document.getElementById("formulaScreen");
+const $displayDiv = document.getElementById("display");
+
+//buttons
+const $clearBtn = document.getElementById("clear");
+const $divideBtn = document.getElementById("divide");
+const $multiplyBtn = document.getElementById("multiply");
+const $addBtn = document.getElementById("add");
+const $subtractBtn = document.getElementById("subtract");
+const $equalsBtn = document.getElementById("equals");
+
+//numbers
+const $numbersBtns = document.getElementsByClassName("num-btn");
